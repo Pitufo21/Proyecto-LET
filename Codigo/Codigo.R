e04618410303e8d61b25fd628b26b4792c134afd
Pitufo21 <matias.alarcon@uc.cl>
2022-11-02 22:21:01 -0300

    02/11/22

--- Codigo/Codigo.R
+++ Codigo/Codigo.R
@@ -14,11 +14,12 @@ datos%>%
 ggplot(aes(x= Player,y= `W%`))+
   geom_col(fill="green", col = "gray40" )+ coord_flip()
 
-?ggplot
 
-A <- c("variables","Player","w%","KDA","CSPM","GD10","KP")
-B <- c("Descripcion","Nombre del jugador","Porcentajes de victorias respecto a las derrotas",
+
+Variables <- c("Player","w%","KDA","CSPM","GD10","KP")
+Descripcion <- c("Nombre del jugador","Porcentajes de victorias respecto a las derrotas",
        "Puntaje calculado por la suma de los asesinatos + asistencias(ayudas de asesinatos), dividido por las muertes del jugador","Cantidad de minions cosechados por minuto",
        "diferencia de oro al minuto 10 respecto al rival","Porcentaje de participacion de asesinatos(Asesinatos+asistencias del jugador divido en el total de asesinatos del equipo)")
-cbind(A,B)%>% 
+c <- cbind(Variables,Descripcion)%>%as.data.frame()%>%
   gt()
+c
